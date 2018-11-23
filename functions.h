813721bc180425fda8a4af0058f2e6a650671fb6
Zach Nett <zachary.nett@wsu.edu>
2018-11-22 22:46:59 -0800

    Added comment header.

--- functions.h
+++ functions.h
@@ -1,3 +1,11 @@
+/***************************************************
+Programmer: Zach Nett
+Class: CptS 121 Fall 2018
+Date: November 22 2018
+Purpose: An implementation of C interview questions.
+GitHub Link: https://github.com/zjnett/CptS-121-PA8
+****************************************************/
+
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
