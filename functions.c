b30045c9f743e17f11b18257941414300312be64
Zach Nett <zachary.nett@wsu.edu>
2018-12-05 22:39:10 -0800

    Added comment documentation.

--- functions.c
+++ functions.c
@@ -1,5 +1,7 @@
 #include "functions.h"
 
+//Function my_str_n_cat.
+//Uses pointer arithmetic to append one string onto another for n characters.
 char * my_str_n_cat(char *dest, const char *src, int n) {
 	while (*dest != '\0') {
 		dest++;
@@ -16,6 +18,10 @@ char * my_str_n_cat(char *dest, const char *src, int n) {
 	return dest;
 }
 
+//Function binary_search.
+//In a list of integers, searches for a target value by continually splitting the list in half.
+//If the target value is found, it returns the index of that target value in the array.
+//Else, it returns -1 (impossible index).
 int binary_search(int list[], int size, int target) {
 	int left = 1, right = size, targetIndex = -1, mid = 0;
 	int found = 0;
@@ -33,6 +39,9 @@ int binary_search(int list[], int size, int target) {
 	return targetIndex;
 }
 
+//Function bubble_sort.
+//Takes an array of strings and swaps addresses of strings based on alphabetical ASCII comparisons.
+//Smaller ASCII values will "bubble" to the top of the array as it is sorted right-to-left.
 void bubble_sort(char * arr[], int numStrings) {
 	int markerU = numStrings, markerC = 1, i = 0;
 	for (i = 0; i < markerU; i++);
@@ -52,6 +61,10 @@ void bubble_sort(char * arr[], int numStrings) {
 }
 
 
+//Function is_palindrome.
+//Removes spaces from string, then uses pointer arithmetic to compare the converging value of str[0] (through recursion & pointer arithmetic) and the end of the string.
+//If they're ever not the same, then the word is not a palindrome.
+//Returns 1 if string is a palindrome, 0 if not.
 //Admittedly a more elegant solution to this would be to call an external function to check if the passed in string has spaces,
 //and another one to remove those spaces. However, I wanted to keep all assigned sub-problems on this assignment to their own functions.
 int is_palindrome(char strOrg[], int length, int hasRemovedSpaces) {
@@ -90,8 +103,11 @@ int is_palindrome(char strOrg[], int length, int hasRemovedSpaces) {
 	}
 }
 
+//Function sum_primes.
 //That being said about recursion and trying to keep every problem in its own function...
 //I'm feeling really lazy.
+//Checks if n is prime. If so, adds it to the recursive call that converges downwards from n to 2.
+//Eventually returns n, or the sum of the prime numbers.
 int sum_primes(unsigned int n) {
 	int sum = 0;
 	if (n == 2) {
@@ -104,10 +120,11 @@ int sum_primes(unsigned int n) {
 	return sum_primes(n - 1) + sum;
 }
 
+//Function isPrime. (can't you tell when I've named functions/variables and used recommended method/variable names? I really don't know why I hate underscores so much.
+//					 ... except in constants/finals/etc.)
+//Returns 1 if number n is prime, 0 if not. Essentially divides odd n by every odd number between 3 and n and checks if it is evenly divisible or not.
+//If so, obviously it's not prime, so it returns 0.
 int isPrime(unsigned int n) {
-	if (n <= 1) {
-		return 0;
-	}
 	if (n % 2 == 0 && n > 2) {
 		return 0;
 	}
@@ -119,6 +136,11 @@ int isPrime(unsigned int n) {
 	return 1;
 }
 
+//Function maximum_occurrences.
+//Reads in a string. Counts all occurrences of each character in the string, and then determines the maximum of those occurrences.
+//Returns value through pointers to a integer and a character, where each corresponding value is kept track of in a parallel array.
+//Frequencies are also calculated based on the number of occurrences of each character divided by the total number of characters, and stored in the
+//parameter array of Occurrences structures.
 void maximum_occurrences(char *str, Occurrences occurArr[], int *max_occurrences, char *ch) {
 	int count[26] = { 0 };
 	char temp[26] = { '\0' };
@@ -126,7 +148,8 @@ void maximum_occurrences(char *str, Occurrences occurArr[], int *max_occurrences
 	int next = 0, nextInt = 0, c = 0, hold = 0, total = 0;
 	while (*str != '\0') {
 		c = 0;
-		for (int i = 0; i < 10; i++) {
+		//100 is an arbitrary hardcoded string length, since total hasn't been calculated yet.
+		for (int i = 0; i < 100; i++) {
 			if (*str == temp[i]) {
 				hold = i;
 				c++;
@@ -142,7 +165,7 @@ void maximum_occurrences(char *str, Occurrences occurArr[], int *max_occurrences
 	}
 
 	//calculate frequencies
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < sizeof(occurArr) / sizeof(Occurrences); i++) {
 		if (count[i] != 0) {
 			occurArr[i].num_occurences = count[i] + 1;
 			occurArr[i].frequency = (double)(count[i] + 1) / total;
@@ -160,6 +183,7 @@ void maximum_occurrences(char *str, Occurrences occurArr[], int *max_occurrences
 			maxIndex = i;
 		}
 	}
+
 	*max_occurrences = max + 1;
 	*ch = temp[maxIndex];
 }
--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include "functions.h"
 
 int main(void) {
+	printf("************* PA8 DRIVER CODE: *************\n");
 	//strcat Driver Code
 	char cat[100] = "cat";
 	my_str_n_cat(cat, "dog", 2);
+	printf("cat + 2 characters of dog = ");
 	puts(cat);
 
 	//Binary Search Driver Code
 	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 15 };
-	printf("%d\n", binary_search(arr, 10, 15));
+	printf("Index of 15 in array: %d\n", binary_search(arr, 10, 15));
+	printf("Index of 400 in array: %d (not found)\n", binary_search(arr, 10, 400));
 
 	//Bubble Sort Driver Code
 	char w1[5] = "cat";
@@ -18,6 +21,7 @@ int main(void) {
 	int * list[4] = { w1, w2, w3, w4 };
 
 	//Prints list prior to sort
+	printf("Unsorted List:\n");
 	for (int i = 0; i < 4; i++) {
 		puts(list[i]);
 	}
@@ -25,12 +29,14 @@ int main(void) {
 	bubble_sort(list, 4);
 	
 	//Prints list after sort
+	printf("Sorted List:\n");
 	for (int i = 0; i < 4; i++) {
 		puts(list[i]);
 	}
 
 	//Palindrome Driver Code
-	printf("Is palindrome: %s\n", is_palindrome("r ac  ec ar", 7, 0) ? "true" : "false");
+	printf("Is 'r ac  ec ar' a palindrome: %s\n", is_palindrome("r ac  ec ar", 7, 0) ? "true" : "false");
+	printf("Is 'ticocat' a palindrome: %s\n", is_palindrome("ticocat", 7, 0) ? "true" : "false");
 
 	//Prime Driver Code
 	printf("Sum of primes from 2 to 10: %d\n", sum_primes(10));
@@ -40,7 +46,9 @@ int main(void) {
 	int occurrences = 0;
 	char ch = '\0';
 	maximum_occurrences("test string", array, &occurrences, &ch);
-	printf("%c has maximum occurrences in string, with %d occurrences.\n", ch, occurrences);
+	printf("%c has maximum occurrences in string 'test string', with %d occurrences.\n", ch, occurrences);
+	maximum_occurrences("apples", array, &occurrences, &ch);
+	printf("%c has maximum occurrences in string 'apples', with %d occurrences.\n", ch, occurrences);
 
 	return 0;
 }
